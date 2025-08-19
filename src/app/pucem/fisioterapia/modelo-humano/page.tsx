7eab01fbf56c717504a0c7736db7326b2f209e4d
STIXGT <stiveguano@gmail.com>
2025-08-19 02:40:06 -0500

    Add model 3d.

--- src/app/pucem/fisioterapia/modelo-humano/page.tsx
+++ src/app/pucem/fisioterapia/modelo-humano/page.tsx
@@ -8,7 +8,7 @@ import { BioDigitalEmbedded } from '@/components/BioDigitalEmbedded';
 export default function FisioterapiaPage() {
   return (
     <div className="container mx-auto py-10 space-y-6">
-      <BioDigitalEmbedded />
+      <BioDigitalEmbedded/>
     </div>
   );
 }
